941fc49bb0187cb2e9f59e8cd6050f4fe19bcc6d
Arminas Saltenis <arminas.saltenis@gmail.com>
2022-02-01 21:53:10 +0200

    Habitapp: Adds scrollview, removes tailwind from git

--- src/screens/AddItemScreen.jsx
+++ src/screens/AddItemScreen.jsx
@@ -1,4 +1,4 @@
-import { SafeAreaView, Text } from 'react-native';
+import { SafeAreaView, ScrollView, Text } from 'react-native';
 import {useTailwind} from 'tailwind-rn';
 import Widget from '../components/shared/Widget';
 
@@ -6,23 +6,25 @@ export default function LoadingScreen({navigation}) {
     const tailwind = useTailwind();
 
     return (
-        <SafeAreaView style={tailwind('flex items-center justify-center mx-4')}>
-            <Widget 
-                title="Create Habit"
-                height="36"
-                description="Create a habit that you want to track"
-                image={require(`./../assets/images/sunny_day.png`)} 
-            />
-            <Widget
-                title="Create Reward"
-                height="36"
-                description="Create a reward that you would like to redeem after sticking to your habits"
-            />
-            <Widget 
-                title="Create Milestone"
-                height="36"
-                description="Create a milestone that you would love to reach"
-            />
+        <SafeAreaView style={tailwind('flex items-center justify-center h-full mx-4')}>
+            <ScrollView>
+                <Widget 
+                    title="Create Habit"
+                    height="52"
+                    description="Create a habit that you want to track"
+                    image={require(`./../assets/images/sunny_day.png`)} 
+                />
+                <Widget
+                    title="Create Reward"
+                    height="52"
+                    description="Create a reward that you would like to redeem after sticking to your habits"
+                />
+                <Widget 
+                    title="Create Milestone"
+                    height="52"
+                    description="Create a milestone that you would love to reach"
+                />
+            </ScrollView>
         </SafeAreaView>
     )
 }
